d68b988c38dd89e329a88d4278ee9c805e4c3677
Yi Yang @ Alecto <ahyangyi@gmail.com>
2021-07-18 08:16:53 +0000

    Disable automatic resize

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
+// Insert custom CSS
 add_action( 'wp_enqueue_scripts', 'de_finibus_enqueue_styles' );
+
 function de_finibus_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+// Turn off automatic resize
+add_filter( 'big_image_size_threshold', '__return_false' );
