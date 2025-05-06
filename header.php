c497e9ae151e6656c43643b43f1ef1091295e608
Yi Yang @ Alecto <ahyangyi@gmail.com>
2025-05-06 18:52:49 +0000

    Merge in upstream updates to header.php
    (Presumably security fixes that has nothing to do with my single-user
     installation, but still worth fixing)

--- header.php
+++ header.php
@@ -13,7 +13,9 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="theme-color" content="#378aba">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+<link rel="pingback" href="<?php echo esc_url( get_bloginfo( 'pingback_url' ) ); ?>">
+<?php endif; ?>
 <?php
 	cryout_header_hook();
 	wp_head();
@@ -21,17 +23,20 @@
 </head>
 
 <body <?php body_class(); cryout_schema_microdata( 'body' );?>>
+	<?php do_action( 'wp_body_open' ); ?>
 	<?php cryout_body_hook(); ?>
-<div id="site-wrapper">
-	<header id="masthead" class="cryout" <?php cryout_schema_microdata( 'header' ) ?> role="banner">
+	<div id="site-wrapper">
+	<header id="masthead" class="cryout" <?php cryout_schema_microdata( 'header' ) ?>>
 
 		<div id="site-header-main">
 			<div id="site-header-main-inside">
 
+				<?php if ( has_nav_menu( 'primary' ) || ( true == cryout_get_option('kahuna_pagesmenu') ) ) { ?>
 				<nav id="mobile-menu">
-					<span id="nav-cancel"><i class="icon-cancel"></i></span>
 					<?php cryout_mobilemenu_hook(); ?>
+					<button id="nav-cancel"><i class="icon-cancel"></i></button>
 				</nav> <!-- #mobile-menu -->
+				<?php } ?>
 
 				<div id="branding">
 					<?php cryout_branding_hook();?>
@@ -41,10 +46,12 @@
 					<?php cryout_header_socials_hook();?>
 				</div>
 
-				<a id="nav-toggle"><i class="icon-menu"></i></a>
+				<?php if ( has_nav_menu( 'primary' ) || ( true == cryout_get_option('kahuna_pagesmenu') ) ) { ?>
+				<button id="nav-toggle" aria-label="<?php esc_attr_e( 'Primary Menu', 'kahuna' ) ?>"><i class="icon-menu"></i></button>
 				<nav id="access" role="navigation"  aria-label="<?php esc_attr_e( 'Primary Menu', 'kahuna' ) ?>" <?php cryout_schema_microdata( 'menu' ); ?>>
 					<?php cryout_access_hook();?>
 				</nav><!-- #access -->
+				<?php } ?>
 
 			</div><!-- #site-header-main-inside -->
 		</div><!-- #site-header-main -->
